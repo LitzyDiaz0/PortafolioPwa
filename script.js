35384e0ddf81e15335f61fc030094828a730fe38
LitzyDiaz0 <2137000110@utna.edu.mx>
2024-12-08 18:56:00 -0600

    nuevo

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/serviceworker.js')
-    .then(registration => {
-        console.log('Service Worker registrado con éxito:', registration);
-    })
-    .catch(error => {
-        console.error('Error al registrar el Service Worker:', error);
-    });
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("./service-worker.js")
+        .then((registration) => {
+            console.log("Service Worker registrado con éxito:", registration);
+        })
+        .catch((error) => {
+            console.error("Error al registrar el Service Worker:", error);
+        });
 }
--- serviceworker.js
+++ serviceworker.js
@@ -1,82 +1,84 @@
-
 const CACHE_NAME = "V1_cache_PWA_IDGS";
-
-urlsToCache = [
-    '/',
-    '/css/style.css',
-    '/js/scripts.js',
-    '/assets/favicon.ico',
-    '/assets/img/animeli.png',
-    '/assets/img/carrusel1.jpeg',
-    '/assets/img/carrusel2.jpeg',
-    '/assets/img/carrusel3.jpeg',
-    '/assets/img/carrusel4.jpeg',
-    '/assets/img/carrusel5.jpeg',
-    '/assets/img/carrusel6.jpeg',
-    '/assets/img/carrusel7.jpeg',
-    '/assets/img/cursos.png',
-    '/assets/img/Develop.png',
-    '/assets/img/iea.png',
-    '/assets/img/img1.png',
-    '/assets/img/img2.png',
-    '/assets/img/img3.png',
-    '/assets/img/img4.png',
-    '/assets/img/leds.png',
-    '/assets/img/portafolio1.png',
-    '/assets/img/smarth.png',
-    '/assets/img/yo.jpg',
-    '/img/fav-icon-60x60.png',
-    '/img/fav-icon-96x96.png',
-    '/img/fav-icon-120x120.png',
-    '/img/fav-icon-180x180.png',
-    '/img/fav-icon-192x192.png',
-    '/img/fav-icon-512x512.png',
-    '/img/favicon-16x16.png',
-    '/img/favicon-32x32.png',
-    'index.html',
-    'serviceworker.js',
-    'manifest.json',
-    'script.js',    
+const urlsToCache = [
+    "./", // Asegúrate de que esta sea la raíz
+    "./index.html",
+    "./css/style.css",
+    "./js/scripts.js",
+    "./assets/favicon.ico",
+    "./assets/img/animeli.png",
+    "./assets/img/carrusel1.jpeg",
+    "./assets/img/carrusel2.jpeg",
+    "./assets/img/carrusel3.jpeg",
+    "./assets/img/carrusel4.jpeg",
+    "./assets/img/carrusel5.jpeg",
+    "./assets/img/carrusel6.jpeg",
+    "./assets/img/carrusel7.jpeg",
+    "./assets/img/cursos.png",
+    "./assets/img/Develop.png",
+    "./assets/img/iea.png",
+    "./assets/img/img1.png",
+    "./assets/img/img2.png",
+    "./assets/img/img3.png",
+    "./assets/img/img4.png",
+    "./assets/img/leds.png",
+    "./assets/img/portafolio1.png",
+    "./assets/img/smarth.png",
+    "./assets/img/yo.jpg",
+    "./img/fav-icon-60x60.png",
+    "./img/fav-icon-96x96.png",
+    "./img/fav-icon-120x120.png",
+    "./img/fav-icon-180x180.png",
+    "./img/fav-icon-192x192.png",
+    "./img/fav-icon-512x512.png",
+    "./img/favicon-16x16.png",
+    "./img/favicon-32x32.png",
+    "./manifest.json",
 ];
 
-//Funcion de instalacion
-//almacena el nombre y los archivos que van a ir guardados en cache
-self.addEventListener('install', e => {
+// Evento de instalación
+self.addEventListener("install", (e) => {
     e.waitUntil(
-    caches.open(CACHE_NAME)
-        .then(cache => {
-        console.log('Cache abierta y recursos agregados');
-        return cache.addAll(urlsToCache);
-        })
-        .then(() => self.skipWaiting())
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log("Archivos en caché:");
+                return cache.addAll(urlsToCache);
+            })
+            .catch((err) => console.error("Error al agregar archivos al caché:", err))
     );
 });
 
-self.addEventListener('activate', e => {
+// Evento de activación
+self.addEventListener("activate", (e) => {
     const allowedCaches = [CACHE_NAME];
     e.waitUntil(
-    caches.keys()
-        .then(cacheNames => {
-        return Promise.all(
-            cacheNames.map(cache => {
-            if (!allowedCaches.includes(cache)) {
-                return caches.delete(cache);
-            }
-            })
-        );
-        })
-        .then(() => self.clients.claim())
+        caches.keys()
+            .then((cacheNames) =>
+                Promise.all(
+                    cacheNames.map((cache) => {
+                        if (!allowedCaches.includes(cache)) {
+                            console.log("Caché eliminado:", cache);
+                            return caches.delete(cache);
+                        }
+                    })
+                )
+            )
+            .then(() => self.clients.claim())
     );
 });
 
-self.addEventListener('fetch', e => {
+// Evento de fetch
+self.addEventListener("fetch", (e) => {
     e.respondWith(
-    caches.match(e.request)
-        .then(response => {
-        if (response) {
-            return response; // Devuelve el recurso desde la caché si existe
-        }
-        return fetch(e.request).catch(() => new Response('No disponible'));
-        })
+        caches.match(e.request)
+            .then((response) => {
+                // Devuelve el recurso desde la caché o realiza un fetch
+                return response || fetch(e.request);
+            })
+            .catch(() => {
+                // Página de respaldo si no hay conexión y el recurso no está en caché
+                if (e.request.mode === "navigate") {
+                    return caches.match("./index.html");
+                }
+            })
     );
 });
